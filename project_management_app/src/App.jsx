73feb94c77ed135c8161210eb49844efc0148047
jerenteria <renteria.juan96@gmail.com>
2024-02-22 22:38:09 -0800

    made projects selectable

--- project_management_app/src/App.jsx
+++ project_management_app/src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react';
 import SideBar from './components/SideBar';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
+import SelectedProject from './components/SelectedProject';
 
 function App() {
   const [projectState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: []
   })
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  };
 
   function handleStartAddProject() {
     // create arrow function to update state so we dont lose prev state
@@ -17,6 +27,17 @@ function App() {
         selectedProjectId: null,
       };
     });
+  };
+
+  // allows cancel button to work by resetting selectedProjectId to 'undefined' so that the screen goes back to 'no project selected'
+  function handleCancelAddProject() {
+        // create arrow function to update state so we dont lose prev state
+        setProjectsState(prevState => { // null === no projected created / undefined === not adding project and no project selected
+          return {
+            ...prevState,
+            selectedProjectId: undefined,
+          };
+        });
   };
 
   function handleAddProject(projectData) {
@@ -34,12 +55,13 @@ function App() {
     });
   };
 
+  // find element in an array by id
+  const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId);
 
-
-  let content; 
+  let content = <SelectedProject project={selectedProject} />; 
   // null === no projected created / undefined === not adding project and no project selected
   if(projectState.selectedProjectId === null) { 
-    content = <NewProject onAdd={handleAddProject} />
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
   } else if(projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
@@ -47,7 +69,7 @@ function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       {/* get the projects by getting the project state and the array of projects */}
-      <SideBar onStartAddProject={handleStartAddProject} projects={projectState.projects} />
+      <SideBar onStartAddProject={handleStartAddProject} projects={projectState.projects} onSelectProject={handleSelectProject} />
       {content}
     </main>
   );
--- project_management_app/src/components/NewProject.jsx
+++ project_management_app/src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import Input from "./Input";
 import { useRef } from "react";
 import Modal from './Modal';
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
 
   const title = useRef();
@@ -36,14 +36,14 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} button="Close">
-        <h2>Invalid Input</h2>
-        <p>You entered invalid data</p>
-        <p>Please make sure you provide valid data</p>
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">You entered invalid data</p>
+        <p className="text-stone-600 mb-4">Please make sure you provide valid data</p>
       </Modal>
       <div className="w-[35-rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
               Cancel
             </button>
           </li>
